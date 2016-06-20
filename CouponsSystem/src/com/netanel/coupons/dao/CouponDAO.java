1838c974ae5decb17f666d96cd9ea642da6280cd
attaln <attaln@ISSAATTALNL7C.corp.emc.com>
2016-06-20 16:26:12 +0300

    started coupon

--- CouponsSystem/src/com/netanel/coupons/dao/CouponDAO.java
+++ CouponsSystem/src/com/netanel/coupons/dao/CouponDAO.java
@@ -1,5 +1,22 @@
 package com.netanel.coupons.dao;
 
+import java.util.Set;
+import com.netanel.coupons.jbeans.Coupon;
+import com.netanel.coupons.jbeans.CouponType;
+
 public interface CouponDAO {
+	public long createCoupon(Coupon coupon);
+	
+	public void removeCoupon(Coupon coupon);
+	public void removeCoupon(long id);
+	public void removeCoupon(String couponName);
+	
+	public void updateCoupon(Coupon coupon);
+	
+	public Coupon getCoupon(long id);
+	
+	public Set<Coupon> getAllCoupons();
+	
+	public Set<Coupon> getCouponByType(CouponType couponType);
 
 }
---
+++ CouponsSystem/src/com/netanel/coupons/dao/db/CouponDbDAO.java
@@ -0,0 +1,59 @@
+package com.netanel.coupons.dao.db;
+
+import java.util.Set;
+
+import com.netanel.coupons.dao.CouponDAO;
+import com.netanel.coupons.jbeans.Coupon;
+import com.netanel.coupons.jbeans.CouponType;
+
+public class CouponDbDAO implements CouponDAO {
+
+	@Override
+	public long createCoupon(Coupon coupon) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeCoupon(Coupon coupon) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeCoupon(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeCoupon(String couponName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCoupon(Coupon coupon) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Coupon getCoupon(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Coupon> getAllCoupons() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Coupon> getCouponByType(CouponType couponType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

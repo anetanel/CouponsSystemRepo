3ea1d1b4d25217d1730eb7e70022f1c61fa7d8cf
netanel <netanel@MINI>
2016-10-12 18:09:10 +0300

    admin service done

--- CouponSystemWeb/src/com/netanel/coupons/web/filter/LoginFilter.java
+++ CouponSystemWeb/src/com/netanel/coupons/web/filter/LoginFilter.java
@@ -22,6 +22,7 @@ public class LoginFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//TODO: remove system outs
 		System.out.println("Filtered!");
 
 		HttpServletRequest req = (HttpServletRequest) request;
--- CouponSystemWeb/src/com/netanel/coupons/web/services/AdminService.java
+++ CouponSystemWeb/src/com/netanel/coupons/web/services/AdminService.java
@@ -1,15 +1,18 @@
 package com.netanel.coupons.web.services;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import com.netanel.coupons.crypt.Password;
@@ -17,44 +20,134 @@ import com.netanel.coupons.exception.DAOException;
 import com.netanel.coupons.facades.AdminFacade;
 import com.netanel.coupons.jbeans.Company;
 import com.netanel.coupons.jbeans.Coupon;
+import com.netanel.coupons.jbeans.Customer;
 
 @Path("admin")
 public class AdminService {
 
+	//
+	// Attributes
+	//
 	@Context
 	HttpServletRequest request;
 
 	private static final String FACADE = "FACADE";
 
+	//
+	// Constructors
+	//
 	public AdminService() {
 	}
 
+	
+	//
+	// Functions
+	//
 	@POST
 	@Path("createcompany")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void createCompany(Map<String, String> map) throws DAOException {
-		AdminFacade facade = getFacade();
-		Company company = new Company(map.get("name"), 
-				new Password(map.get("password").toCharArray()), 
-				map.get("email"),
+	public void createCompany(Map<String, String> companyMap) throws DAOException {
+		Company company = new Company(companyMap.get("name"), 
+				new Password(companyMap.get("password").toCharArray()), 
+				companyMap.get("email"),
 				new HashSet<Coupon>());
-		facade.createCompany(company);
+		getFacade().createCompany(company);
 	}
-		
+	
+	@DELETE
+	@Path("deletecompany")
+	//TODO: deal with IO exception
+	public void deleteCompany(@QueryParam("compId") long compId) throws DAOException, IOException {
+		getFacade().deleteCompany(getCompany(compId));
+	}
+	
+	@POST
+	@Path("updatecompany")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCompanyDetails(Map<String, String> companyMap) throws DAOException {
+		Company company = new Company(Long.parseLong(companyMap.get("id")),
+				companyMap.get("name"), 
+				new Password(companyMap.get("password").toCharArray()), 
+				companyMap.get("email"),
+				null);
+		getFacade().updateCompanyDetails(company);
+	}
+	
+	@GET
+	@Path("getcompanybyid")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Company getCompany(@QueryParam("compId") long compId) throws DAOException {
+		return getFacade().getCompany(compId);
+	}
+	
+	@GET
+	@Path("getcompanybyname")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Company getCompany(@QueryParam("compName") String compName) throws DAOException {
+		return getFacade().getCompany(compName);
+	}
+	
 	@GET
 	@Path("getallcompanies")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Set<Company> getAllCompanies() throws DAOException {
-		AdminFacade facade = getFacade();
-		return facade.getAllCompanies();
+		return getFacade().getAllCompanies();
 	}
 
+	
+	@GET
+	@Path("getcustomerbyid")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Customer getCustomer(@QueryParam("custId") long custId) throws DAOException {
+		return getFacade().getCustomer(custId);
+	}
+	
+	@GET
+	@Path("getcustomerbyname")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Customer getCustomer(@QueryParam("custName") String custName) throws DAOException {
+		return getFacade().getCustomer(custName);
+	}
+	
+	@GET
+	@Path("getallcustomers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Set<Customer> getAllCustomers() throws DAOException {
+		return getFacade().getAllCustomers();
+	}
+	
+	@POST
+	@Path("createcustomer")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void createCustomer(Map<String, String> customerMap) throws DAOException {
+		Customer customer = new Customer(customerMap.get("name"), 
+				new Password(customerMap.get("password").toCharArray()), 
+				new HashSet<Coupon>());
+		getFacade().createCustomer(customer);
+	}
+	
+	@DELETE
+	@Path("deletecustomer")
+	public void deleteCustomer(@QueryParam("custId") long custId) throws DAOException {
+		getFacade().deleteCustomer(getCustomer(custId));
+	}
+	
+	@POST
+	@Path("updatecustomer")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCustomerDetails(Map<String, String> customerMap) throws DAOException {
+		Customer customer = new Customer(Long.parseLong(customerMap.get("id")),
+				customerMap.get("name"), 
+				new Password(customerMap.get("password").toCharArray()), 
+				null);
+		getFacade().updateCustomerDetails(customer);
+	}
+	
 	@GET
 	@Path("whoami")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String whoAmI() throws DAOException {
-		AdminFacade facade = getFacade();
-		return facade.toString();
+		return getFacade().toString();
 	}
 
 	private AdminFacade getFacade() throws DAOException {
